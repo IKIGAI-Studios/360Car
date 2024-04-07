3fa709f660cee274236725fa84895e2348d3ad3a
SrCosmicCat <gatitosplus000@gmail.com>
2024-04-07 07:46:09 -0600

    Agrega rutas y enlaces de navegación en la aplicación

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { CochesComponent } from './components/coches/coches.component';
+import { TransaccionesComponent } from './components/transacciones/transacciones.component';
+import { ClientesComponent } from './components/clientes/clientes.component';
 
 const routes: Routes = [
   {
     path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'coches',
+    component: CochesComponent,
+  },
+  {
+    path: 'transacciones',
+    component: TransaccionesComponent,
+  },
+  {
+    path: 'clientes',
+    component: ClientesComponent,
+  },
+  {
+    path: '**',
     component: HomeComponent
   }
 ];
