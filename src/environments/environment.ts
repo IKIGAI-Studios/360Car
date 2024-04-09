8698ac88f78cc17abe405b6d7ae0e6f65b338127
SrCosmicCat <106451141+SrCosmicCat@users.noreply.github.com>
2024-04-09 14:45:41 -0600

    Delete environments info

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,5 @@
 export const environment = {
     production: false,
     firebaseConfig: {
-        apiKey: "AIzaSyAMKh_kgg0H6tNsbFMzG6bnrUO_GXcEPZU",
-        authDomain: "car-b8820.firebaseapp.com",
-        projectId: "car-b8820",
-        storageBucket: "car-b8820.appspot.com",
-        messagingSenderId: "312572004386",
-        appId: "1:312572004386:web:78a5bf3d16d316157b227b"
     }
 };
